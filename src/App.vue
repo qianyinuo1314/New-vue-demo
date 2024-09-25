84a8d897bca060aac2e2f26d090ec83e350f8cb1
1455427048@qq.com <11175443+tuan-t@user.noreply.gitee.com>
2024-09-25 11:46:20 +0800

    新建文件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">首页</router-link> 
+    <!-- <router-link to="/about">About</router-link> -->
   </nav>
+  <section>
+     <router-link to="/">首页</router-link> 
+  </section>
+  <footer>首页我的</footer>
   <router-view/>
+  
 </template>
 
 <style>
@@ -13,10 +18,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
 nav {
   padding: 30px;
+   
 }
 
 nav a {
@@ -27,4 +34,5 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
